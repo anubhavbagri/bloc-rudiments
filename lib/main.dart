e712012a5a54fceca89d275158e1efcfa4af4953
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-07-06 22:41:24 +0530

    [ADD] BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:bloc_rudiments/blocs/internet_bloc/internet_bloc.dart';
 import 'package:bloc_rudiments/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,10 +14,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'BLoC App',
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+    return BlocProvider(
+      create: (context) => InternetBloc(),
+      child: const MaterialApp(
+        title: 'BLoC App',
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      ),
     );
   }
 }
