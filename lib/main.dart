78e2ff70d7977bad932ddc4d184689ea530530e4
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-07-06 23:39:57 +0530

    [REFACTOR] states with enum

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bloc_rudiments/blocs/internet_bloc/internet_bloc.dart';
+import 'package:bloc_rudiments/cubits/internet_cubit.dart';
 import 'package:bloc_rudiments/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => InternetBloc(),
+      create: (context) => InternetCubit(),
       child: const MaterialApp(
         title: 'BLoC App',
         debugShowCheckedModeBanner: false,
